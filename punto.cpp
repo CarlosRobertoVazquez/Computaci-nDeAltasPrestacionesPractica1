c88af2495ae32708c43d27bf77000aa282bbc680
CarlosRobertoVazquez <carlosrobertovazquezsalinas@gmail.com>
2026-02-26 13:08:00 +0100

    start of redaccting the introducction to the proyect

---
+++ punto.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+
+class punto{ 
+    int nDimensiones;
+    std::vector<int> datos; 
+
+    punto(int nDimensiones, )
+
+}
